b8b15f50d94ce6af318b79a2762cc7f71f2697b5
lucas <lucasmtrachta90@gmail.com>
2025-04-12 17:12:11 -0300

    primer commit de ramadeprueba2 hacemos las primeras lineas y despues guardamos y volvemos para ver las mismas lineas volvemos desde ramadeprueba3 para volver a ver los cambios

---
+++ js/pruebacambioderama.js
@@ -0,0 +1,5 @@
+
+
+"estamos aqui para crear un archivo de java y cambiarnos de rama sin perder los cambios ya hechos el archivo al que estamos dejando para ir a la nueva rama"
+
+"fffffffffffffffffffffffffffffffffffffffffffffff"
